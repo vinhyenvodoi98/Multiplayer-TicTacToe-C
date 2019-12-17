4769ecbc692cca50e620f0f10f89c742ed013d9c
hoang <vinhyenvodoi98@gmail.com>
2019-12-17 09:43:15 +0700

    fix : multi player ip

--- client.c
+++ client.c
@@ -280,8 +280,7 @@ int main(int argc, char *argv[])
 
   // assign IP, PORT
   servaddr.sin_family = AF_INET;
-  servaddr.sin_addr.s_addr = INADDR_ANY;
-  // servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  servaddr.sin_addr.s_addr = inet_addr(ipAddress);
   servaddr.sin_port = htons(PORT);
 
   // connect the client socket to server socket
